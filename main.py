still a bug in training with normalization code. Data values appear to be huge for some values. Loss doesn't converge.

main.py
@@ -124,9 +124,14 @@ if __name__ == '__main__':
     test_data=normalize(test_data,train_mean,train_std)
     test_labels=normalize(test_labels,label_mean, label_std)
 
-      # Get number of classes
-    num_classes = train_labels.size(-1)
-    print("\n")
+
+    # Convert Numpy Arrays to PyTorch Tensors
+    train_data = torch.from_numpy(train_data).float()
+    train_labels = torch.from_numpy(train_labels).float()
+    dev_data=torch.from_numpy(dev_data).float()
+    dev_labels=torch.from_numpy(dev_labels).float()
+    test_data=torch.from_numpy(test_data).float()
+    test_labels=torch.from_numpy(test_labels).float()
 
     # Create PyTorch Dataset
     train_set = Dataset(train_data, train_labels, mode)
@@ -137,6 +142,10 @@ if __name__ == '__main__':
     train_generator = data.DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
     dev_generator = data.DataLoader(dataset=dev_set, batch_size=512, shuffle=True)
     test_generator = data.DataLoader(dataset=test_set, batch_size=512, shuffle=True)
+
+     # Get number of classes
+    num_classes = train_labels.size(-1)
+    print("\n")
 
     print('###################################################################################\n\r'
           '# Dataset\n\r'

modules/data.py
@@ -40,6 +40,9 @@ def normalize(dat, mean, std):
     return dat
 
 def unnormalize(dat, mean, std):
+    rep=dat.shape[2]/mean.shape[0] # there is 1 mean for each input sensor value, repeat it for each element in sequence in data
+    mean=mean.repeat(rep)
+    std=std.repeat(rep)
     dat= dat * std + mean
     return dat
 
@@ -78,7 +81,7 @@ def load_data(filepath, cw_plen, cw_flen, pw_len, pw_off, seq_len, stride=1, med
         medfilt: median filter window, 0 to disable
 
     Returns:
-        Unnormalized torch.Tensor(data).float() tensors
+        Unnormalized numpy arrays
         input_data:  indexed by [sample, sequence, # sensor inputs * cw_len]
         label_data:  indexed by [sample, sequence, # output sensor values * pw_len]
         mean_train_data, std_train_data, mean_target_data, std_target_data: the means and stds of training and target data.
@@ -205,9 +208,5 @@ def load_data(filepath, cw_plen, cw_flen, pw_len, pw_off, seq_len, stride=1, med
     # print("Postion Min: ", position_max)
     # print("Angle Min:   ", angle_min)
     # print("Angle Max:   ", angle_max)
-
-    # Convert Numpy Arrays to PyTorch Tensors
-    data_new = torch.from_numpy(data_new).float()
-    target_new = torch.from_numpy(target_new).float()
 
     return data_new, target_new, m_tr, s_tr, m_tst, s_tst

test_plot.py
@@ -5,7 +5,7 @@ import torch.utils.data.dataloader
 import numpy as np
 import random as rnd
 from modules.util import load_normalization
-from modules.data import load_data
+from modules.data import load_data, normalize, unnormalize
 import matplotlib.pyplot as plt
 
 # import matplotlib.pylab as pylab
@@ -70,19 +70,18 @@ if __name__ == '__main__':
     # both are torch tensors
 
     # Get Mean and Std of Train Data, to use normalize test data and unnormalize it for plotting
-    mean_train_data, std_train_data=load_normalization(savepath) # we need to unnormalize the predictions to get the predictions in input units
+    mean_train_data, std_train_data, mean_label_data, std_label_data=load_normalization(savepath) # we need to unnormalize the predictions to get the predictions in input units
+    test_data_norm = normalize(test_data, mean_train_data, std_train_data)
+    test_labels_norm = normalize(test_labels, mean_label_data, std_label_data)
 
     # Get number of classes
     rnn_output_size = test_labels.size(-1)
-    print("\n")
-
-
-    print('###################################################################################\n\r'
+    print('\n###################################################################################\n\r'
           '# Dataset\n\r'
           '###################################################################################')
     print("Dim: (num_sample, look_back_len, feat_size)")
-    print("Test data size:         ", test_data.size())
-    print("Test prediction size:   ", test_labels.size())
+    print("Test data size:         ", test_data_norm.size())
+    print("Test prediction size:   ", test_labels_norm.size())
     print("\n\r")
 
     ########################################################
@@ -91,8 +90,8 @@ if __name__ == '__main__':
     print("Loading network...")
 
     # Network Dimension
-    rnn_inp_size = test_data.size(-1)
-    rnn_output_size = test_labels.size(-1)
+    rnn_inp_size = test_data_norm.size(-1)
+    rnn_output_size = test_labels_norm.size(-1)
     print("RNN input size:        ", rnn_inp_size)
     print("RNN output size:       ", rnn_output_size)
 
@@ -137,10 +136,10 @@ if __name__ == '__main__':
     print("Starting inference...")
 
     ########################################################################
-    test_data = test_data[:, 0, :].unsqueeze(1) # raw data in a time series, unsqueeze keeps as 3d
-    test_data = test_data.transpose(0, 1) # put seq len as first, sample as 2nd, to normalize
-    test_data_norm = (test_data - mean_train_data) / std_train_data # to input to RNN for prediction
-    # test_sample_norm = quantizeTensor(test_sample_norm, aqi, aqf, 1) # TODO add check for quantization
+    # test_data = test_data[:, 0, :].unsqueeze(1) # raw data in a time series, unsqueeze keeps as 3d
+    # test_data = test_data.transpose(0, 1) # put seq len as first, sample as 2nd, to normalize
+    # test_data_norm = (test_data - mean_train_data) / std_train_data # to input to RNN for prediction
+    # # test_sample_norm = quantizeTensor(test_sample_norm, aqi, aqf, 1) # TODO add check for quantization
     test_sample_norm = test_data_norm.transpose(0, 1) # flip for input to RNN
     if args.cuda:
         test_sample_norm = test_data_norm.cuda() # move data to cuda