Fixed errors in normalization and test_plot bugs
Wrongly used np.repeat. Should have used np.tile in normalization methods.

main.py
@@ -37,18 +37,18 @@ if __name__ == '__main__':
 
     # Hyperparameters
     stride = args.stride
-    cw_plen = args.cw_plen            # Length of history in timesteps used to train the network
-    cw_flen = args.cw_flen            # Length of future in timesteps to predict
-    pw_len = args.pw_len              # Offset of future in timesteps to predict
-    pw_off = args.pw_off              # Length of future in timesteps to predict
-    seq_len = args.seq_len            # Sequence length
-    lr = args.lr                      # Learning rate
-    batch_size = args.batch_size      # Mini-batch size
-    num_epochs = args.num_epochs      # Number of epoches to train the network
+    cw_plen = args.cw_plen  # Length of history in timesteps used to train the network
+    cw_flen = args.cw_flen  # Length of future in timesteps to predict
+    pw_len = args.pw_len  # Offset of future in timesteps to predict
+    pw_off = args.pw_off  # Length of future in timesteps to predict
+    seq_len = args.seq_len  # Sequence length
+    lr = args.lr  # Learning rate
+    batch_size = args.batch_size  # Mini-batch size
+    num_epochs = args.num_epochs  # Number of epoches to train the network
     mode = args.mode
-    train_file=args.train_file
-    val_file=args.val_file
-    test_file=args.test_file
+    train_file = args.train_file
+    val_file = args.val_file
+    test_file = args.test_file
     qa = args.qa
     qw = args.qw
 
@@ -83,7 +83,7 @@ if __name__ == '__main__':
     wqf = args.wqf
     # Save and Log
     str_target_variable = 'cart-pole'
-    save_path={}
+    save_path = {}
     if mode == 0:  # Pretrain on GRU
         str_net_arch = str(num_rnn_layers) + 'L-' + str(rnn_hid_size) + 'H-'
         str_windows = str(cw_plen) + 'CWP-' + str(cw_flen) + 'CWF-' + str(pw_len) + 'PWL-' + str(pw_off) + 'PWO-'
@@ -102,71 +102,123 @@ if __name__ == '__main__':
         str_net_arch = str(num_rnn_layers) + 'L-' + str(rnn_hid_size) + 'H-'
         str_windows = str(cw_plen) + 'CWP-' + str(cw_flen) + 'CWF-' + str(pw_len) + 'PWL-' + str(pw_off) + 'PWO-'
         filename = str_net_arch + str(rnn_type) + '-' + str_windows + str_target_variable
-        pretrain_model_path = './save/' + str_net_arch + 'GRU' + '-' + str_windows + str_target_variable + '.pt' #TODO probably a bug in this filename, has extra parts
+        pretrain_model_path = './save/' + str_net_arch + 'GRU' + '-' + str_windows + str_target_variable + '.pt'  # TODO probably a bug in this filename, has extra parts
         logpath = './log/' + filename + '_' + str(th_x) + '.csv'
         savepath = './save/' + filename + '_' + str(th_x) + '.pt'
-
 
     ########################################################
     # Create Dataset
     ########################################################
-    train_data, train_labels, train_mean, train_std, label_mean, label_std = load_data(train_file, cw_plen, cw_flen, pw_len, pw_off, seq_len, args.stride, args.med_filt)
-    dev_data, dev_labels, _, _, _, _ = load_data(val_file, cw_plen, cw_flen, pw_len, pw_off, seq_len, args.stride, args.med_filt)
-    test_data, test_labels, _, _, _, _ = load_data(test_file, cw_plen, cw_flen, pw_len, pw_off, seq_len, args.stride, args.med_filt)
+    train_features, train_targets, _, mean_train_features, std_train_features, mean_train_targets, std_train_targets = \
+        load_data(train_file, cw_plen, cw_flen, pw_len, pw_off, seq_len, args.stride, args.med_filt)
+    dev_features, dev_targets, _, _, _, _, _ = \
+        load_data(val_file, cw_plen, cw_flen, pw_len, pw_off, seq_len, args.stride, args.med_filt)
+    test_features, test_targets, _, _, _, _, _ = \
+        load_data(test_file, cw_plen, cw_flen, pw_len, pw_off, seq_len, args.stride, args.med_filt)
 
-    save_normalization(savepath,train_mean,train_std, label_mean, label_std)
+    save_normalization(savepath, mean_train_features, std_train_features, mean_train_targets, std_train_targets)
 
     # normalize all data by training set values
-    train_data=normalize(train_data,train_mean,train_std)
-    train_labels=normalize(train_labels,label_mean, label_std)
-    dev_data=normalize(dev_data,train_mean,train_std)
-    dev_labels=normalize(dev_labels,label_mean, label_std)
-    test_data=normalize(test_data,train_mean,train_std)
-    test_labels=normalize(test_labels,label_mean, label_std)
+    train_features = normalize(train_features, mean_train_features, std_train_features)
+    train_targets = normalize(train_targets, mean_train_targets, std_train_targets)
+    dev_features = normalize(dev_features, mean_train_features, std_train_features)
+    dev_targets = normalize(dev_targets, mean_train_targets, std_train_targets)
+    test_features = normalize(test_features, mean_train_features, std_train_features)
+    test_targets = normalize(test_targets, mean_train_targets, std_train_targets)
 
+    print("train_features: ", train_features.shape)
+    print("mean_train_features: ", mean_train_features)
+    print("std_train_features: ", std_train_features)
 
     # Convert Numpy Arrays to PyTorch Tensors
-    train_data = torch.from_numpy(train_data).float()
-    train_labels = torch.from_numpy(train_labels).float()
-    dev_data=torch.from_numpy(dev_data).float()
-    dev_labels=torch.from_numpy(dev_labels).float()
-    test_data=torch.from_numpy(test_data).float()
-    test_labels=torch.from_numpy(test_labels).float()
+    train_features = torch.from_numpy(train_features).float()
+    train_targets = torch.from_numpy(train_targets).float()
+    dev_features = torch.from_numpy(dev_features).float()
+    dev_targets = torch.from_numpy(dev_targets).float()
+    test_features = torch.from_numpy(test_features).float()
+    test_targets = torch.from_numpy(test_targets).float()
 
     # Create PyTorch Dataset
-    train_set = Dataset(train_data, train_labels, mode)
-    dev_set = Dataset(dev_data, dev_labels, mode)
-    test_set = Dataset(test_data, test_labels, mode)
+    train_set = Dataset(train_features, train_targets, mode)
+    dev_set = Dataset(dev_features, dev_targets, mode)
+    test_set = Dataset(test_features, test_targets, mode)
 
     # Create PyTorch dataloaders for train and dev set
     train_generator = data.DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
-    dev_generator = data.DataLoader(dataset=dev_set, batch_size=512, shuffle=True)
-    test_generator = data.DataLoader(dataset=test_set, batch_size=512, shuffle=True)
+    dev_generator = data.DataLoader(dataset=dev_set, batch_size=512, shuffle=False)
+    test_generator = data.DataLoader(dataset=test_set, batch_size=512, shuffle=False)
 
-     # Get number of classes
-    num_classes = train_labels.size(-1)
+    # Get number of classes
+    num_classes = train_targets.size(-1)
     print("\n")
 
     print('###################################################################################\n\r'
-          '# Dataset\n\r'
+          '# Dataset (Normalized)\n\r'
           '###################################################################################')
-    print("Dim: (num_sample, look_back_len, feat_size)")
-    print("Train data size:  ", train_data.size())
-    print("Train label size: ", train_labels.size())
-    print("Test data size:   ", dev_data.size())
-    print("Test label size:  ", dev_labels.size())
+    print("# Train Data  | Size:   %s                            \n"
+          "#             | Min:    %f                            \n"
+          "#             | Mean:   %f                            \n"
+          "#             | Median: %f                            \n"
+          "#             | Max:    %f                            \n"
+          "#             | Std:    %f                            \n"
+          "#-----------------------------------------------------\n"
+          "# Dev   Data  | Size:   %s                            \n"
+          "#             | Min:    %f                            \n"
+          "#             | Mean:   %f                            \n"
+          "#             | Median: %f                            \n"
+          "#             | Max:    %f                            \n"
+          "#             | Std:    %f                            \n"
+          "#-----------------------------------------------------\n"
+          "# Test  Data  | Size:   %s                            \n"
+          "#             | Min:    %f                            \n"
+          "#             | Mean:   %f                            \n"
+          "#             | Median: %f                            \n"
+          "#             | Max:    %f                            \n"
+          "#             | Std:    %f                            \n"
+          "#-----------------------------------------------------\n"
+          "# Train Label | Size:   %s                            \n"
+          "#             | Min:    %f                            \n"
+          "#             | Mean:   %f                            \n"
+          "#             | Median: %f                            \n"
+          "#             | Max:    %f                            \n"
+          "#             | Std:    %f                            \n"
+          "#-----------------------------------------------------\n"
+          "# Dev   Label | Size:   %s                            \n"
+          "#             | Min:    %f                            \n"
+          "#             | Mean:   %f                            \n"
+          "#             | Median: %f                            \n"
+          "#             | Max:    %f                            \n"
+          "#             | Std:    %f                            \n"
+          "#-----------------------------------------------------\n"
+          "# Test  Label | Size:   %s                            \n"
+          "#             | Min:    %f                            \n"
+          "#             | Mean:   %f                            \n"
+          "#             | Median: %f                            \n"
+          "#             | Max:    %f                            \n"
+          "#             | Std:    %f                            \n"
+          "#-----------------------------------------------------\n"
+          % (str(list(train_features.size())), train_features.min(), train_features.mean(), train_features.median(), train_features.max(), train_features.std(),
+             str(list(dev_features.size())), dev_features.min(), dev_features.mean(), dev_features.median(), dev_features.max(), dev_features.std(),
+             str(list(test_features.size())), test_features.min(), test_features.mean(), test_features.median(), test_features.max(), test_features.std(),
+             str(list(train_targets.size())), train_targets.min(), train_targets.mean(), train_targets.median(), train_targets.max(), train_targets.std(),
+             str(list(dev_targets.size())), dev_targets.min(), dev_targets.mean(), dev_targets.median(), dev_targets.max(), dev_targets.std(),
+             str(list(test_targets.size())), test_targets.min(), test_targets.mean(), test_targets.median(), test_targets.max(), test_targets.std()
+            )
+          )
+
     print("\n\r")
-    # Data Range
-    angle_min = 0
-    angle_max = 2 * math.pi
-    position_min = -655
-    position_max = 1644
+
+    # # Data Range
+    # angle_min = 0
+    # angle_max = 2 * math.pi
+    # position_min = -655
+    # position_max = 1644
 
     print('###################################################################################\n\r'
           '# Network\n\r'
           '###################################################################################')
     # Network Dimension
-    rnn_inp_size = train_data.size(-1)
+    rnn_inp_size = train_features.size(-1)
     print("rnn_inp_size             = ", rnn_inp_size)
     print("rnn_hid_size             = ", rnn_hid_size)
     print("num_rnn_layers           = ", num_rnn_layers)
@@ -208,7 +260,8 @@ if __name__ == '__main__':
         net.load_state_dict(new_state_dict)
 
     # Move network to GPU
-    if args.cuda:  net = net.cuda()
+    if args.cuda:
+        net = net.cuda()
 
     # Print parameter count
     params = 0
@@ -272,7 +325,7 @@ if __name__ == '__main__':
         train_batches = 0
         sp_dx = 0
         sp_dh = 0
-        net.set_quantize_act(1)  # Enable quantization in activation (only applies to DeltaGRU)
+        net.set_quantize_act(0)  # Enable quantization in activation (only applies to DeltaGRU)
         net.set_eval_sparsity(0)  # Don't evaluate sparsity for faster training
 
         for batch, labels in tqdm(train_generator):  # Iterate through batches
@@ -311,7 +364,7 @@ if __name__ == '__main__':
             train_batches += 1  # Accumulate count so we can calculate mean later
 
         # Quantize the RNN weights after every epoch
-        net = quantize_rnn(net, wqi, wqf, 1)
+        net = quantize_rnn(net, wqi, wqf, qw)
 
         # Evaluate Weight Sparsity
         n_nonzero_weight_elem = 0
@@ -327,7 +380,7 @@ if __name__ == '__main__':
         # Validation - Iterate batches
         ###########################################################################################################
         net = net.eval()
-        net.set_quantize_act(1)
+        net.set_quantize_act(qa)
         net.set_eval_sparsity(1)  # Enable sparsity evaluation
 
         dev_loss = 0
@@ -358,7 +411,7 @@ if __name__ == '__main__':
         # Test - Iterate batches
         ###########################################################################################################
         net = net.eval()
-        net.set_quantize_act(1)
+        net.set_quantize_act(qa)
         net.set_eval_sparsity(1)  # Enable sparsity evaluation
 
         nz_dx = 0
@@ -436,7 +489,6 @@ if __name__ == '__main__':
         dict_log['sp_W'] = sp_W
         dict_log['sp_dx'] = sp_dx
         dict_log['sp_dh'] = sp_dh
-
 
         print('Epoch: %3d of %3d | '
               'Time: %s | '

modules/data.py
@@ -8,7 +8,8 @@ from scipy.signal import medfilt as medfilt
 
 # TODO check value, might not be 4096 full scale. It does rotate all the way, is 0 at left, 2000 to right, about 3210 vertical up, and -947 hanging vertically
 RAD_PER_ANGLE_ADC = 2 * math.pi / 4096  # a complete rotation of the potentiometer is mapped to this many ADC counts
-POSITION_LIMIT=4000 # position encoder has limit +/- POSITION_MAX
+POSITION_LIMIT = 4000  # position encoder has limit +/- POSITION_MAX
+
 
 class Dataset(data.Dataset):
     def __init__(self, data, labels, retrain):
@@ -33,18 +34,20 @@ class Dataset(data.Dataset):
 
 
 def normalize(dat, mean, std):
-    rep=dat.shape[2]/mean.shape[0] # there is 1 mean for each input sensor value, repeat it for each element in sequence in data
-    mean=mean.repeat(rep)
-    std=std.repeat(rep)
-    dat= (dat - mean) / std
+    rep = int(dat.shape[-1] / mean.shape[0])  # there is 1 mean for each input sensor value, repeat it for each element in sequence in data
+    mean = np.tile(mean, rep)
+    std = np.tile(std, rep)
+    dat = (dat - mean) / std
     return dat
 
+
 def unnormalize(dat, mean, std):
-    rep=dat.shape[2]/mean.shape[0] # there is 1 mean for each input sensor value, repeat it for each element in sequence in data
-    mean=mean.repeat(rep)
-    std=std.repeat(rep)
-    dat= dat * std + mean
+    rep = int(dat.shape[-1] / mean.shape[0])  # there is 1 mean for each input sensor value, repeat it for each element in sequence in data
+    mean = np.tile(mean, rep)
+    std = np.tile(std, rep)
+    dat = dat * std + mean
     return dat
+
 
 def computeNormalization(dat: numpy.array):
     '''
@@ -63,9 +66,10 @@ def computeNormalization(dat: numpy.array):
     # so that the values don't change with different sample rate.)
     m = np.mean(dat, 0)
     s = np.std(dat, 0)
-    m[0]=m[1]=0
-    s[0]=s[1]=1 # handle some things specially, like sin/cos that should not be touched
-    return m,s
+    m[0] = m[1] = 0
+    s[0] = s[1] = 1  # handle some things specially, like sin/cos that should not be touched
+    return m, s
+
 
 def load_data(filepath, cw_plen, cw_flen, pw_len, pw_off, seq_len, stride=1, med_filt=3):
     '''
@@ -83,130 +87,115 @@ def load_data(filepath, cw_plen, cw_flen, pw_len, pw_off, seq_len, stride=1, med
     Returns:
         Unnormalized numpy arrays
         input_data:  indexed by [sample, sequence, # sensor inputs * cw_len]
-        label_data:  indexed by [sample, sequence, # output sensor values * pw_len]
-        mean_train_data, std_train_data, mean_target_data, std_target_data: the means and stds of training and target data.
+        targets:  indexed by [sample, sequence, # output sensor values * pw_len]
+        mean_features, std_features, mean_targets_data, std_targets_data: the means and stds of training and raw_targets data.
             These are vectors with length # of sensorss
 
-        The last dimension of input_data and label_data is organized by
+        The last dimension of input_data and targets is organized by
          all sensors for sample0, all sensors for sample1, .....all sensors for sampleN, where N is the prediction length
     '''
 
     # Load dataframe
     df = pd.read_csv(filepath)
 
-    # time,deltaTimeMs,angle,position,angleTarget,angleErr,positionTarget,positionErr,angleCmd,positionCmd,motorCmd,actualMotorCmd
+    # time,deltaTimeMs,angle,position,angletargets,angleErr,positiontargets,positionErr,angleCmd,positionCmd,motorCmd,actualMotorCmd
     # 172.2985134124756,4.787921905517578,1699,-418,3129,-1428.0,0,-418.0,573146.4030813494,-8360.0,7055,0
-    # Collect data
-    train_data = []
-    all_data = []
 
-    time=df.time.to_numpy()
-    deltaTimeMs=df.deltaTimeMs.to_numpy()
+    time = df.time.to_numpy()
+    deltaTimeMs = df.deltaTimeMs.to_numpy()
 
     # Get Raw Data
-    angle = df.angleErr.to_numpy() # simplify the pole angle to just angle error, so it is zero centered around presumed vertical position
+    angle = df.angleErr.to_numpy()  # simplify the pole angle to just angle error, so it is zero centered around presumed vertical position
     # desired vertical angle might be incorrectly set, resulting in large position offset that balances position and angle control
-    angle = angle*RAD_PER_ANGLE_ADC # angle has range -pi to +pi, centered very closely around 0 during balancing
-    if med_filt>0:
-        angle=medfilt(angle, med_filt)
-    position = df.position.to_numpy()/POSITION_LIMIT # leave cart position as absolute position since the target position might change with time and the values
-                                        # for control might depend on position of cart
+    angle = angle * RAD_PER_ANGLE_ADC  # angle has range -pi to +pi, centered very closely around 0 during balancing
+    position = df.position.to_numpy() / float(POSITION_LIMIT)  # leave cart position as absolute position since the raw_targets position might change with time and the values
+    # for control might depend on position of cart
     # position has range < +/-1
-    if med_filt>0:
-        position=medfilt(position, med_filt)
+
+    # Median Filter
+    if med_filt > 0:
+        angle = medfilt(angle, med_filt)
+        position = medfilt(position, med_filt)
 
     # project angle onto x and y since angle is a rotational variable with 2pi cut that we cannot fit properly and does not represent gravity and lateral acceleration well.
-    sinAngle=np.sin(angle)
-    cosAngle=np.cos(angle)
+    sinAngle = np.sin(angle)
+    cosAngle = np.cos(angle)
     # compute temporal derivatives from state data
-    averageDeltaTMs=deltaTimeMs.mean() # TODO this is approximate derivative since sample rate varied a bit around 5ms
+    averageDeltaTMs = deltaTimeMs.mean()  # TODO this is approximate derivative since sample rate varied a bit around 5ms
     # TODO consider using a better controlled derivative that enforces e.g. total variation constraint
-    actualMotorCmd = df.actualMotorCmd.to_numpy() # zero-centered motor speed command
+    actualMotorCmd = df.actualMotorCmd.to_numpy()  # zero-centered motor speed command
 
     # Derive Other Data
-    # dAngle = np.gradient(angle,averageDeltaTMs)
-    # ddAngle = np.gradient(dAngle, edge_order=1)  # same for accelerations
-    dCosAngle = np.gradient(cosAngle,averageDeltaTMs)
-    dSinAngle = np.gradient(sinAngle,averageDeltaTMs)
+    dAngle = np.gradient(angle,averageDeltaTMs)
+    ddAngle = np.gradient(dAngle, edge_order=1)  # same for accelerations
+    dCosAngle = np.gradient(cosAngle, averageDeltaTMs)
+    dSinAngle = np.gradient(sinAngle, averageDeltaTMs)
     ddCosAngle = np.gradient(dCosAngle, averageDeltaTMs)
     ddSinAngle = np.gradient(dSinAngle, averageDeltaTMs)
     dPosition = np.gradient(position, averageDeltaTMs)
     ddPosition = np.gradient(dPosition, edge_order=1)
 
-    # Train Data
-    # train_data.append(angle)
-    # train_data.append(dAngle)
-    # train_data.append(ddAngle)
-    train_data.append(sinAngle)
-    train_data.append(cosAngle)
-    train_data.append(dSinAngle)
-    train_data.append(dCosAngle)
-    # train_data.append(ddSinAngle)
-    # train_data.append(ddCosAngle)
-    train_data.append(position)
-    train_data.append(dPosition)
-    # train_data.append(ddPosition)
-    train_data.append(actualMotorCmd)
+    # Features (Train Data)
+    raw_features = []
+    # raw_features.append(angle)
+    # raw_features.append(dAngle)
+    # raw_features.append(ddAngle)
+    raw_features.append(sinAngle)
+    raw_features.append(cosAngle)
+    raw_features.append(dSinAngle)
+    raw_features.append(dCosAngle)
+    # raw_features.append(ddSinAngle)
+    # raw_features.append(ddCosAngle)
+    raw_features.append(position)
+    raw_features.append(dPosition)
+    # raw_features.append(ddPosition)
+    raw_features.append(actualMotorCmd)
+    raw_features = np.vstack(raw_features).transpose()  # raw_features indexed by [sample, input sensor/control]
 
-    train_data = np.vstack(train_data).transpose()   # train_data indexed by [sample, input sensor/control]
+    # targetss (Label Data)
+    raw_targets = []
+    raw_targets.append(sinAngle)
+    raw_targets.append(cosAngle)
+    raw_targets.append(dSinAngle)
+    raw_targets.append(dCosAngle)
+    # raw_targets.append(angle)
+    # raw_targets.append(dAngle)
+    raw_targets.append(position)
+    # raw_targets.append(dPosition)
+    raw_targets = np.vstack(raw_targets).transpose()  # raw_targets indexed by [sample, sensor]
 
-    target = []
-    target.append(sinAngle)
-    target.append(cosAngle)
-    target.append(dSinAngle)
-    target.append(dCosAngle)
-    # target.append(angle)
-    # target.append(dAngle)
-    target.append(position)
-    target.append(dPosition)
-    target = np.vstack(target).transpose()   # target indexed by [sample, sensor]
-
-    train_sample = train_data[1:-1, :]  # TODO why throw away first and last time point, maybe CSV file incomplete
-    # print("Sample Size: ", train_data.shape)
-    # print("Train Max: ", np.amax(train_sample))
-    # print("Train Min: ", np.amin(train_sample))
-    # print("Test Max: ", np.amax(train_sample))
-    # print("Test Min: ", np.amin(train_sample))
+    # Actual Data for Plotting
+    raw_actual = []
+    raw_actual.append(angle)
+    raw_actual.append(position)
+    actual = np.vstack(raw_actual).transpose()  # raw_actual data indexed by [sample, sensor]
 
     # compute normalization of data now
-    m_tr, s_tr = computeNormalization(train_data)
-    m_tst, s_tst = computeNormalization(target)
-
-    # Print Collected Data Information
-    print('###################################################################################\n\r'
-          '# Collected Data Information\n\r'
-          '###################################################################################')
-    print("train_sample size: ", train_sample.shape)
+    mean_features, std_features = computeNormalization(raw_features)
+    mean_targets, std_targets = computeNormalization(raw_targets)
 
     # Split train data
-    numSamples = train_sample.shape[0]
+    numSamples = raw_features.shape[0]
 
-    data_new = []
-    target_new = []
+    features = []
+    targets = []
     # Iterate from the current timestep to the last timestep that gives the last prediction window
-    for i in range(cw_plen, numSamples - max(cw_flen, pw_len+pw_off) - seq_len):
+    for i in range(cw_plen, numSamples - max(cw_flen, pw_len + pw_off) - seq_len):
         if i % stride is not 0:
             continue
-        data_seq = []
-        label_seq = []
+        raw_feature_seq = []
+        raw_target_seq = []
         for t in range(0, seq_len):
-            data_seq.append(train_sample[i+t-cw_plen:i+t+cw_flen+1, :].ravel())
+            raw_feature_seq.append(raw_features[i + t - cw_plen:i + t + cw_flen + 1, :].ravel())
             # ravel() makes 1d array for sensor values and prediction window length
             # order is row major, with last index changing fastest
             # since last index is sensor, the order is [sensor1t0, sensor2t0, ... sensorNt0, sensor0t1, sensor2t1 ....]
-            label_seq.append(target[i+t+pw_off:i+t+pw_off+pw_len, :].ravel())
-        data_seq = np.stack(data_seq, axis=0)
-        label_seq = np.stack(label_seq, axis=0)
-        data_new.append(data_seq)
-        target_new.append(label_seq)
-    data_new = np.stack(data_new, axis=0) # indexed by [sample, sequence, # sensor inputs * cw_len]
-    target_new = np.stack(target_new, axis=0) # [sample, sequence, # output sensor values * pw_len]
+            raw_target_seq.append(raw_targets[i + t + pw_off:i + t + pw_off + pw_len, :].ravel())
+        raw_feature_seq = np.stack(raw_feature_seq, axis=0)
+        raw_target_seq = np.stack(raw_target_seq, axis=0)
+        features.append(raw_feature_seq)
+        targets.append(raw_target_seq)
+    features = np.stack(features, axis=0)  # indexed by [sample, sequence, # sensor inputs * cw_len]
+    targets = np.stack(targets, axis=0)  # [sample, sequence, # output sensor values * pw_len]
 
-    print("data_new size: ", data_new.shape)
-    print("target_new size: ", target_new.shape)
-    # print("Postion Min: ", position_min)
-    # print("Postion Min: ", position_max)
-    # print("Angle Min:   ", angle_min)
-    # print("Angle Max:   ", angle_max)
-
-    return data_new, target_new, m_tr, s_tr, m_tst, s_tst
+    return features, targets, actual, mean_features, std_features, mean_targets, std_targets

modules/models.py
@@ -84,7 +84,7 @@ class Model(nn.Module):
              )
         self.fc = nn.Sequential(
             nn.Linear(in_features=self.cla_size, out_features=self.cla_size, bias=True),
-            nn.LeakyReLU(),
+            nn.ReLU(),
             nn.Dropout(p=0.5),
             nn.Linear(in_features=self.cla_size, out_features=num_classes, bias=True)
         )

test_plot.py
@@ -28,12 +28,12 @@ if __name__ == '__main__':
     pw_off = args.pw_off  # Length of future in timesteps to predict
     pw_idx = args.pw_idx  # Index of timestep in the prediction window
     seq_len = args.seq_len  # Sequence length
-    test_file=args.test_file
+    test_file = args.test_file
 
     # Plot Settings
     # start_test_tstep = args.start_test_tstep
     # num_test_tstep = args.num_test_tstep
-    plot_len = args.plot_len # Number of timesteps in the plot window
+    plot_len = args.plot_len  # Number of timesteps in the plot window
 
     # Whether test retrain model
     retrain = 0
@@ -64,24 +64,25 @@ if __name__ == '__main__':
     ########################################################
     # Create Dataset
     ########################################################
-    test_data, test_labels,_,_,_,_ = load_data(test_file, cw_plen, cw_flen, pw_len, pw_off, seq_len, args.stride, args.med_filt)
-    # test_data: input sensor and control signals
-    # test_labels: what we want to predict (the sensor data into the future)
+    test_features, test_targets, test_actual, _, _, _, _ = load_data(test_file, cw_plen, cw_flen, pw_len, pw_off, seq_len, args.stride,
+                                                   args.med_filt)
+    test_features = test_features[:, 0, :]  # Get a continuous time series
+    # test_features: input sensor and control signals
+    # test_targets: what we want to predict (the sensor data into the future)
     # both are torch tensors
 
     # Get Mean and Std of Train Data, to use normalize test data and unnormalize it for plotting
-    mean_train_data, std_train_data, mean_label_data, std_label_data=load_normalization(savepath) # we need to unnormalize the predictions to get the predictions in input units
-    test_data_norm = normalize(test_data, mean_train_data, std_train_data)
-    test_labels_norm = normalize(test_labels, mean_label_data, std_label_data)
+    mean_train_features, std_train_features, mean_train_targets, std_train_targets \
+        = load_normalization(savepath)  # we need to unnormalize the predictions to get the predictions in input units
+    test_features_norm = normalize(test_features, mean_train_features, std_train_features)
+    test_targets_norm = normalize(test_targets, mean_train_targets, std_train_targets)
 
-    # Get number of classes
-    rnn_output_size = test_labels.size(-1)
     print('\n###################################################################################\n\r'
           '# Dataset\n\r'
           '###################################################################################')
     print("Dim: (num_sample, look_back_len, feat_size)")
-    print("Test data size:         ", test_data_norm.size())
-    print("Test prediction size:   ", test_labels_norm.size())
+    print("Test data size:         ", test_features_norm.shape)
+    print("Test prediction size:   ", test_targets_norm.shape)
     print("\n\r")
 
     ########################################################
@@ -90,8 +91,8 @@ if __name__ == '__main__':
     print("Loading network...")
 
     # Network Dimension
-    rnn_inp_size = test_data_norm.size(-1)
-    rnn_output_size = test_labels_norm.size(-1)
+    rnn_inp_size = test_features_norm.shape[-1]
+    rnn_output_size = test_targets_norm.shape[-1]
     print("RNN input size:        ", rnn_inp_size)
     print("RNN output size:       ", rnn_output_size)
 
@@ -127,8 +128,8 @@ if __name__ == '__main__':
     net.load_state_dict(new_state_dict)
 
     # Move network to GPU
-    if args.cuda:
-        net = net.cuda()
+    # if args.cuda:
+    #     net = net.cuda()
 
     ########################################################
     ########################################################
@@ -136,25 +137,29 @@ if __name__ == '__main__':
     print("Starting inference...")
 
     ########################################################################
-    # test_data = test_data[:, 0, :].unsqueeze(1) # raw data in a time series, unsqueeze keeps as 3d
-    # test_data = test_data.transpose(0, 1) # put seq len as first, sample as 2nd, to normalize
-    # test_data_norm = (test_data - mean_train_data) / std_train_data # to input to RNN for prediction
+    # actual_data = test_features[:, 0, :]  # Prepare actual data
+    # test_features = test_features.transpose(0, 1) # put seq len as first, sample as 2nd, to normalize
+    # test_features_norm = (test_features - mean_train_features) / std_train_features # to input to RNN for prediction
     # # test_sample_norm = quantizeTensor(test_sample_norm, aqi, aqf, 1) # TODO add check for quantization
-    test_sample_norm = test_data_norm.transpose(0, 1) # flip for input to RNN
-    if args.cuda:
-        test_sample_norm = test_data_norm.cuda() # move data to cuda
+    test_sample_norm = torch.from_numpy(test_features_norm).float()  # Convert Numpy to PyTorch
+    test_sample_norm = test_sample_norm.unsqueeze(1)  # Convert Numpy to PyTorch
+    # test_sample_norm = test_sample_norm.transpose(0, 1)  # flip for input to RNN
+    # if args.cuda:
+    #     test_sample_norm = test_sample_norm.cuda()  # move data to cuda
 
-    net = net.eval() # set to eval mode
-    y_pred, _, _ = net(test_sample_norm) # run samples through RNN, results in
-    y_pred = y_pred.squeeze().cpu().detach().numpy() # Convert tensor to numpy mat [sample, state]
+    net = net.eval()  # set to eval mode
+    y_pred, _, _ = net(test_sample_norm)  # run samples through RNN, results in
+    y_pred = y_pred.squeeze().cpu().detach().numpy()  # Convert tensor to numpy mat [sample, state]
+    y_pred = unnormalize(y_pred, mean_train_targets, std_train_targets)  # Unnormalize network prediction
 
-    print('###################################################################################\n\r'
-          '# Evaluation Information\n\r'
-          '###################################################################################')
-
-    print("Test Sample Size:      ", test_sample_norm.size())
-    print("Test Output Size:      ", y_pred.shape)
-    print("Test Real Data Size:   ", test_labels.shape)
+    # print('###################################################################################\n\r'
+    #       '# Evaluation Information\n\r'
+    #       '###################################################################################')
+    #
+    # print("Test Sample Size:  ", test_sample_norm.size())
+    # print("Test Output Size:  ", y_pred.shape)
+    # print("Actual Data Size:  ", actual_data.shape)
+    # print("Test Label Size:   ", test_targets.shape)
 
     ########################################################################
     # Draw a plot of RNN input and output
@@ -163,18 +168,21 @@ if __name__ == '__main__':
     num_test_tstep = test_sample_norm.size(0)
 
     # Get Ground Truth
-    t_curr = cw_plen  # Put the initial timestep at the first timestep after the first possible context window
+    t_start = cw_plen  # Put the initial timestep at the first timestep after the first possible context window
     t_start = 0
-    ts_actual = np.arange(t_start, t_start + num_test_tstep) # timesteps to show actual input data
-    angle_actual = test_data[:, ts_actual, 0].squeeze() # is normalized data of input angle
-    position_actual = test_data[:, ts_actual, 3].squeeze() # normalized position
-    # Get Angle Prediction
-    ts_pred = np.arange(t_curr + pw_off, t_curr + pw_off + num_test_tstep) # prediction timesteps, not quite the same since there is offset and window
-    y_pred = np.reshape(y_pred, (num_test_tstep, pw_len, -1))  # ???? TODO why doesn't this use the ts_pred?? Reshape to add the feature dimension (timestep, pw_len, feat)
-    sin_pred = np.squeeze(y_pred[t_start:t_start + num_test_tstep, 0, 0]) # TODO same here, why not use ts_pred?
+    ts_actual = np.arange(t_start, t_start + num_test_tstep)  # timesteps to show actual input data
+    angle_actual = test_actual[ts_actual, 0].squeeze()  # is normalized data of input angle
+    position_actual = test_actual[ts_actual, 1].squeeze()  # normalized position
+
+    # Get Prediction
+    ts_pred = np.arange(t_start + pw_off,
+                        t_start + pw_off + num_test_tstep)  # prediction timesteps, not quite the same since there is offset and window
+    y_pred = np.reshape(y_pred, (num_test_tstep, pw_len,
+                                 -1))  # ???? TODO why doesn't this use the ts_pred?? Reshape to add the feature dimension (timestep, pw_len, feat)
+    sin_pred = np.squeeze(y_pred[t_start:t_start + num_test_tstep, 0, 0])  # TODO same here, why not use ts_pred?
     cos_pred = np.squeeze(y_pred[t_start:t_start + num_test_tstep, 0, 1])
-    angle_pred = np.arctan2(sin_pred, cos_pred) # compute angle from sin and cos
-    position_pred = y_pred[t_start:t_start + num_test_tstep, 0, 1] # prediction of normalized position
+    angle_pred = np.arctan2(sin_pred, cos_pred)  # compute angle from sin and cos
+    position_pred = y_pred[t_start:t_start + num_test_tstep, 0, 4]  # prediction of normalized position
 
     # Plot angle error
     fig1, (ax1, ax2) = plt.subplots(2, 1, figsize=(14, 8))
@@ -197,8 +205,6 @@ if __name__ == '__main__':
     ax2.tick_params(axis='both', which='major', labelsize=20)
     ax2.legend(fontsize=14)
 
-    plt.show()
-
     ########################################################################
     # Slider
     ########################################################################
@@ -216,15 +222,16 @@ if __name__ == '__main__':
     ts_context = np.arange(t_start, t_start + cw_plen)
     # TODO angle from sin/cos actual and prediction
     ts_pred = np.arange(t_curr + pw_off, t_curr + pw_off + pw_len)
-    sin_context = test_data[:, t_start:t_start + cw_plen, 0].squeeze()
-    cos_context = test_data[:, t_start:t_start + cw_plen, 0].squeeze()
+    angle_actual = test_actual[t_start:t_start + plot_len, 0].squeeze()
+    sin_context = test_features[t_start:t_start + cw_plen, 0].squeeze()
+    cos_context = test_features[t_start:t_start + cw_plen, 1].squeeze()
     angle_context = np.arctan2(sin_context, cos_context)
     sin_pred = np.squeeze(y_pred[t_start, :, 0])
-    cos_pred = np.squeeze(y_pred[t_start, :, 0])
+    cos_pred = np.squeeze(y_pred[t_start, :, 1])
     angle_pred = np.arctan2(sin_pred, cos_pred)
-    position_actual = test_data[:, t_start:t_start + plot_len, 3].squeeze() # shouldl already be in radians
-    position_context = test_data[:, t_start:t_start + cw_plen, 3].squeeze() # should already be in radians
-    position_pred = y_pred[t_start, :, 1]
+    position_actual = test_actual[t_start:t_start + plot_len, 1].squeeze()  # shouldl already be in radians
+    position_context = test_features[t_start:t_start + cw_plen, 4].squeeze()  # should already be in radians
+    position_pred = y_pred[t_start, :, 4]
 
     # Draw Plots
     plot1_actual, = ax1.plot(ts_actual, position_actual, 'k.', lw=3, label='Ground Truth')
@@ -235,36 +242,42 @@ if __name__ == '__main__':
     plot2_pred, = ax2.plot(ts_pred, angle_pred, 'r-', lw=2, label='Prediction')
 
     ax1.set_ylabel("Pos. Error (rad)", fontsize=14)
-    ax1.set_yticks(np.arange(-0.1, 1.1, 0.2))
+    ax1.set_yticks(np.arange(-0.1, 0.4, 0.1))
     ax1.legend(fontsize=12)
     ax2.set_ylabel("Ang. Error (rad)", fontsize=14)
     ax2.set_xlabel('Timestep (200 Hz)', fontsize=14)
-    ax2.set_yticks(np.arange(-0.1, 1.1, 0.2))
+    ax2.set_yticks(np.arange(-0.04, 0.05, 0.02))
     ax2.legend(fontsize=12)
     ax1.margins(x=0)
 
     axcolor = 'lightgoldenrodyellow'
     axtstep = plt.axes([0.15, 0.15, 0.75, 0.03], facecolor=axcolor)
 
-
     # Sliders
     a0 = 5
     f0 = 3
     delta_step = 1.0
-    ststep = Slider(axtstep, 'Timestep', cw_plen, num_test_tstep-pw_len-pw_off-plot_len, valinit=f0, valstep=delta_step)
+    ststep = Slider(axtstep, 'Timestep', cw_plen, num_test_tstep - pw_len - pw_off - plot_len, valinit=f0,
+                    valstep=delta_step)
+
 
     def update(val):
-        t_curr = int(ststep.val)  # Put the initial timestep at the first timestep after the first possible context window
-        t_plot = t_curr - cw_plen
-        t_actual = np.arange(t_plot, t_plot + plot_len)
-        t_context = np.arange(t_plot, t_plot + cw_plen)
+        t_curr = int(
+            ststep.val)  # Put the initial timestep at the first timestep after the first possible context window
+        t_start = t_curr - cw_plen
+        t_actual = np.arange(t_start, t_start + plot_len)
+        t_context = np.arange(t_start, t_start + cw_plen)
         t_pred = np.arange(t_curr + pw_off, t_curr + pw_off + pw_len)
-        angle_actual = test_data[:,t_plot:t_plot + plot_len,0].squeeze() # should already be in radians
-        angle_context = test_data[:,t_plot:t_plot + cw_plen,0].squeeze() # should already be in radians
-        angle_pred = np.squeeze(y_pred[t_plot, :, 0])
-        position_actual = test_data[:,t_plot:t_plot + plot_len,3].squeeze() # should already be in radians
-        position_context = test_data[:,t_plot:t_plot + cw_plen,3].squeeze() # should already be in radians
-        position_pred = y_pred[t_plot, :, 1]
+        angle_actual = test_actual[t_start:t_start + plot_len, 0].squeeze()
+        sin_context = test_features[t_start:t_start + cw_plen, 0].squeeze()
+        cos_context = test_features[t_start:t_start + cw_plen, 1].squeeze()
+        angle_context = np.arctan2(sin_context, cos_context)
+        sin_pred = np.squeeze(y_pred[t_start, :, 0])
+        cos_pred = np.squeeze(y_pred[t_start, :, 1])
+        angle_pred = np.arctan2(sin_pred, cos_pred)
+        position_actual = test_actual[t_start:t_start + plot_len, 1].squeeze()  # shouldl already be in radians
+        position_context = test_features[t_start:t_start + cw_plen, 4].squeeze()  # should already be in radians
+        position_pred = y_pred[t_start, :, 4]
         plot1_actual.set_xdata(t_actual)
         plot1_context.set_xdata(t_context)
         plot1_pred.set_xdata(t_pred)
@@ -278,8 +291,8 @@ if __name__ == '__main__':
         plot2_context.set_ydata(angle_context)
         plot2_pred.set_ydata(angle_pred)
         # ax1.set_xticks(t_actual)
-        ax1.set_xlim(xmin=t_plot, xmax=t_plot + plot_len)
-        ax2.set_xlim(xmin=t_plot, xmax=t_plot + plot_len)
+        ax1.set_xlim(xmin=t_start, xmax=t_start + plot_len)
+        ax2.set_xlim(xmin=t_start, xmax=t_start + plot_len)
         # ax2.set_xticks(t_actual)
 
 
@@ -288,8 +301,11 @@ if __name__ == '__main__':
     resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
     button = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
 
+
     def reset(event):
         ststep.reset()
+
+
     button.on_clicked(reset)
 
     plt.show()